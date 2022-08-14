c94d6266fe753a5a94d620c075896a39a610ad8a
l x <lx@192.168.0.108>
2022-08-14 13:57:56 +0800

    fix udp

--- leaf/src/mobile/logger.rs
+++ leaf/src/mobile/logger.rs
@@ -6,25 +6,19 @@ use std::{
 use bytes::BytesMut;
 
 #[cfg(any(target_os = "ios", target_os = "macos"))]
-use super::bindings::{asl_log, ASL_LEVEL_NOTICE};
+//use super::bindings::{asl_log, ASL_LEVEL_NOTICE};
 
 #[cfg(target_os = "android")]
 //use super::bindings::{__android_log_print, android_LogPriority_ANDROID_LOG_VERBOSE};
 
 #[cfg(any(target_os = "ios", target_os = "macos"))]
 fn log_out(data: &[u8]) {
-    unsafe {
-        let s = match ffi::CString::new(data) {
-            Ok(s) => s,
-            Err(_) => return,
-        };
-        asl_log(
-            std::ptr::null_mut(),
-            std::ptr::null_mut(),
-            ASL_LEVEL_NOTICE as i32,
-            s.as_c_str().as_ptr(),
-        )
-    };
+    // unsafe {
+    //     let s = match ffi::CString::new(data) {
+    //         Ok(s) => s,
+    //         Err(_) => return,
+    //     };
+    // };
 }
 
 #[cfg(target_os = "android")]
@@ -51,7 +45,6 @@ impl Write for ConsoleWriter {
     fn write(&mut self, buf: &[u8]) -> io::Result<usize> {
         self.0.extend_from_slice(buf);
         if let Some(i) = memchr::memchr(b'\n', &self.0) {
-            log_out(&self.0[..i]);
             let _ = self.0.split_to(i + 1);
         }
         Ok(buf.len())
