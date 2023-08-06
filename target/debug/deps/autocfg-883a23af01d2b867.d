a62d5bf9b459e89444c6950179ec2e602a3cb9bc
tenon <58762103+tenondvpn@users.noreply.github.com>
2023-08-07 00:07:35 +0800

    suport direct not route

--- target/debug/deps/autocfg-883a23af01d2b867.d
+++
@@ -1,9 +0,0 @@
-d:\work\leaf_new\target\debug\deps\autocfg-883a23af01d2b867.rmeta: C:\Users\Administrator\.cargo\registry\src\index.crates.io-6f17d22bba15001f\autocfg-1.1.0\src\lib.rs C:\Users\Administrator\.cargo\registry\src\index.crates.io-6f17d22bba15001f\autocfg-1.1.0\src\error.rs C:\Users\Administrator\.cargo\registry\src\index.crates.io-6f17d22bba15001f\autocfg-1.1.0\src\version.rs
-
-d:\work\leaf_new\target\debug\deps\libautocfg-883a23af01d2b867.rlib: C:\Users\Administrator\.cargo\registry\src\index.crates.io-6f17d22bba15001f\autocfg-1.1.0\src\lib.rs C:\Users\Administrator\.cargo\registry\src\index.crates.io-6f17d22bba15001f\autocfg-1.1.0\src\error.rs C:\Users\Administrator\.cargo\registry\src\index.crates.io-6f17d22bba15001f\autocfg-1.1.0\src\version.rs
-
-d:\work\leaf_new\target\debug\deps\autocfg-883a23af01d2b867.d: C:\Users\Administrator\.cargo\registry\src\index.crates.io-6f17d22bba15001f\autocfg-1.1.0\src\lib.rs C:\Users\Administrator\.cargo\registry\src\index.crates.io-6f17d22bba15001f\autocfg-1.1.0\src\error.rs C:\Users\Administrator\.cargo\registry\src\index.crates.io-6f17d22bba15001f\autocfg-1.1.0\src\version.rs
-
-C:\Users\Administrator\.cargo\registry\src\index.crates.io-6f17d22bba15001f\autocfg-1.1.0\src\lib.rs:
-C:\Users\Administrator\.cargo\registry\src\index.crates.io-6f17d22bba15001f\autocfg-1.1.0\src\error.rs:
-C:\Users\Administrator\.cargo\registry\src\index.crates.io-6f17d22bba15001f\autocfg-1.1.0\src\version.rs:
