768b33768ee4c9b0b86aa2a9ad113c8abe49fd7d
tenon <58762103+tenondvpn@users.noreply.github.com>
2023-08-07 00:07:15 +0800

    suport direct not route

---
+++ target/debug/deps/lazy_static-853b9f824b4f75bd.d
@@ -0,0 +1,6 @@
+d:\work\leaf_new\target\debug\deps\lazy_static-853b9f824b4f75bd.rmeta: C:\Users\Administrator\.cargo\registry\src\index.crates.io-6f17d22bba15001f\lazy_static-1.4.0\src\lib.rs C:\Users\Administrator\.cargo\registry\src\index.crates.io-6f17d22bba15001f\lazy_static-1.4.0\src\inline_lazy.rs
+
+d:\work\leaf_new\target\debug\deps\lazy_static-853b9f824b4f75bd.d: C:\Users\Administrator\.cargo\registry\src\index.crates.io-6f17d22bba15001f\lazy_static-1.4.0\src\lib.rs C:\Users\Administrator\.cargo\registry\src\index.crates.io-6f17d22bba15001f\lazy_static-1.4.0\src\inline_lazy.rs
+
+C:\Users\Administrator\.cargo\registry\src\index.crates.io-6f17d22bba15001f\lazy_static-1.4.0\src\lib.rs:
+C:\Users\Administrator\.cargo\registry\src\index.crates.io-6f17d22bba15001f\lazy_static-1.4.0\src\inline_lazy.rs:
